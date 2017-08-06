58675affca94811921cdd60a87eca0146fd8bd2d
Summonedbyhonor <mateuszdeptula1@gmail.com>
2017-08-06 13:44:12 +0200

    Added new funct to TheHighestElementInArray

--- src/main/java/TheHighestElementInArray/HEAMain.java
+++ src/main/java/TheHighestElementInArray/HEAMain.java
@@ -25,7 +25,25 @@ public class HEAMain {
         return wynik;
     }
 
+    public static int lowestElementMethod(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę elementów tablicy:");
+        int arrayElements = scanner.nextInt();
+        int[] array = new int[arrayElements];
+        for (int i = 0; i < array.length; i++){
+            System.out.println("Podaj liczbę w komórce " + (i+1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+        int wynik = array[0];
+        for (int i : array){
+            if (wynik > i){
+                wynik = i;
+            }
+        }
+        return wynik;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Największy element w tablicy: " + highestElementMethod());
+        System.out.println("Największy element w tablicy: " + lowestElementMethod());
     }
 }
