287df909cb6dd135c47217eae6bc29b487249664
Summonedbyhonor <mateuszdeptula1@gmail.com>
2017-08-06 11:11:08 +0200

    algorytm

--- src/main/java/TheAbsoluteValue/AbsoluteValue.java
+++ src/main/java/TheAbsoluteValue/AbsoluteValue.java
@@ -6,10 +6,17 @@ import java.util.Scanner;
  * Created by Mateusz on 2017-08-06.
  */
 public class AbsoluteValue {
+
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
 
+        absoluteValueMethod(number);
+    }
+
+    private static void absoluteValueMethod(int number) {
         if (number < 0) {
             System.out.println((-number));
         } else {
